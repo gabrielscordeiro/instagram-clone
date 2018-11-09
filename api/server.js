4c2584d92d6766998985cfb8af3c12334d31996d
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2018-11-09 13:11:30 -0200

    Estrutura inicial da API

---
+++ api/server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const objectId = require('mongodb').ObjectId;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+const port = 8080;
+
+app.listen(port);
+
+
+const db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
+
+console.log(`Servidor rodando na porta ${port}`);
+
+app.get('/', function (req, res) {
+    res.send({
+        msg: 'Olá'
+    });
+});
+
+app.post('/api', function (req, res) {
+    let dados = req.body;
+
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.insert(dados, function (err, response) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(response);
+                }
+                
+                mongoclient.close();
+            });
+        })
+    })
+});
+
+app.get('/api', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.find().toArray(function (err, response) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(response);
+                }
+
+                mongoclient.close();
+            });
+        })
+    })
+});
+
+//Get By ID
+app.get('/api/:id', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.find(objectId(req.params.id)).toArray(function (err, response) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(response);
+                }
+
+                mongoclient.close();
+            });
+        })
+    })
+});
+
+app.put('/api/:id', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.update({
+                _id: objectId(req.params.id)
+            }, {
+                    $set: {
+                        titulo: req.body.titulo
+                    }
+            }, {},
+            function (err, response) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(response);
+                }
+
+                mongoclient.close();
+            });
+        })
+    })
+});
